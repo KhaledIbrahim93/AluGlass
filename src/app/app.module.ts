1c5983ecd271e2f3f89341e3e85e1eaad5c49a42
Khaled Ibrahim <kamel.mohamed@intalio.com>
2024-03-09 18:16:43 +0200

    enhancment in desgin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ImageModule } from 'primeng/image';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { GALLERY_CONFIG, GalleryConfig, GalleryModule } from 'ng-gallery';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,19 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ImageModule,
+    BrowserAnimationsModule,
+    CarouselModule.forRoot(),
+    GalleryModule,
   ],
-  providers: [],
+  providers: [{
+    provide: GALLERY_CONFIG,
+    useValue: {
+      autoHeight: true,
+      imageSize: 'cover'
+    } as GalleryConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GalleryItem, ImageItem } from 'ng-gallery';
+
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  images: GalleryItem[]=[];
+
+  itemsPerSlide = 3;
+  slides = [
+    {image: '../../assets/Imgs/1.jpg'},
+    {image: '../../assets/Imgs/2.jpg'},
+    {image: '../../assets/Imgs/3.jpg'},
+    {image: '../../assets/Imgs/8.jpg'},
+    {image: '../../assets/Imgs/5.jpg'},
+    {image: '../../assets/Imgs/6.jpg'},
+    {image: '../../assets/Imgs/8.jpg'},
+    {image: '../../assets/Imgs/5.jpg'},
+    {image: '../../assets/Imgs/6.jpg'},
+  ];
+
+  ngOnInit() {
+    // Set items array
+    this.images.push( new ImageItem({ src: '../../assets/Imgs/6.jpg', thumb: '../../assets/Imgs/6.jpg' }))
+    this.images.push( new ImageItem({ src: '../../assets/Imgs/7.jpg', thumb: '../../assets/Imgs/7.jpg' }))
+    this.images.push( new ImageItem({ src: '../../assets/Imgs/9.jpg', thumb: '../../assets/Imgs/9.jpg' }))
+    this.images.push( new ImageItem({ src: '../../assets/Imgs/10.jpg', thumb: '../../assets/Imgs/10.jpg' }))
+    this.images.push( new ImageItem({ src: '../../assets/Imgs/3.jpg', thumb: '../../assets/Imgs/3.jpg' }))
+    this.images.push( new ImageItem({ src: '../../assets/Imgs/11.jpg', thumb: '../../assets/Imgs/11.jpg' }))
+    this.images.push( new ImageItem({ src: '../../assets/Imgs/8.jpg', thumb: '../../assets/Imgs/8.jpg' }))
+    this.images.push( new ImageItem({ src: '../../assets/Imgs/9.jpg', thumb: '../../assets/Imgs/9.jpg' }))
+    this.images.push( new ImageItem({ src: '../../assets/Imgs/6.jpg', thumb: '../../assets/Imgs/6.jpg' }))
+
+  }
 
 }
